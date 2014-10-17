8b6c93e704e309133ee3e52321463b5ec351efc8
Niadel <niadel@virginmedia.com>
2014-10-17 16:12:54 +0100

    Filled out Entity.EntityType.

--- Corundum/entities/Player.java
+++ Corundum/entities/Player.java
@@ -4,31 +4,38 @@ import Corundum.exceptions.UnfinishedException;
 import Corundum.utils.interfaces.Commander;
 import Corundum.utils.messaging.MessageReceiver;
 import Corundum.world.Location;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.IChatComponent;
 
 public class Player extends Mob implements Commander, MessageReceiver {
-    private final net.minecraft.entity.player.EntityPlayerMP playerMC;
+    private final EntityPlayerMP playerMC;
 
-    public Player(net.minecraft.entity.player.EntityPlayerMP playerMC) {
+    public Player(EntityPlayerMP playerMC) {
         this.playerMC = playerMC;
     }
 
-    public void addChatMessage(net.minecraft.util.IChatComponent message) {
+    @Override
+    public void addChatMessage(IChatComponent message) {
         playerMC.addChatMessage(message);
     }
 
+    @Override
     public Location getLocation() {
         return new Location(playerMC.getCommandSenderPosition().posX, playerMC.getCommandSenderPosition().posY, playerMC.getCommandSenderPosition().posZ, Corundum.world.World
                 .fromMCWorld((net.minecraft.world.WorldServer) playerMC.worldObj));
     }
 
+    @Override
     public boolean canCommandSenderUseCommand(int permission_level, String command) {
         return playerMC.canCommandSenderUseCommand(permission_level, command);
     }
 
-    public net.minecraft.util.IChatComponent func_145748_c_() {
+    @Override
+    public IChatComponent func_145748_c_() {
         return playerMC.func_145748_c_();
     }
 
+    @Override
     public String getName() {
         return playerMC.getCommandSenderName();
     }
