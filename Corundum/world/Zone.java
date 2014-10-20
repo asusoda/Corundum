5e1e5a11569ef2c3b8f137a251c8f74ac2048282
Niadel <niadel@virginmedia.com>
2014-10-20 20:01:11 +0100

    Added some helper methods to Zone, pretty much completed the Zone constructor.

--- Corundum/world/Zone.java
+++ Corundum/world/Zone.java
@@ -15,7 +15,13 @@ public class Zone {
         // - if location1 and location2 are in different worlds, throw a ZoneCornersInDifferentWorldsException
         // - initialize "low" and "high" as described in the DEV NOTES Javadoc comments above
         if (location1.getWorld() == location2.getWorld()) {
-
+            if (location1.getX() > location2.getX() && location1.getY() > location2.getY() && location1.getZ() > location2.getZ()) {
+                this.high = location1;
+                this.low = location2;
+            } else {
+                this.high = location2;
+                this.low = location1;
+            }
         } else {
             //If the worlds are different, throw an exception.
             throw new ZoneCornersInDifferentWorldsException(location1, location2);
@@ -25,11 +31,27 @@ public class Zone {
     public class ZoneCornersInDifferentWorldsException extends CorundumException {
         private static final long serialVersionUID = -1202714861919120683L;
 
-        public ZoneCornersInDifferentWorldsException(Location location1, Location location2, Object... additional_information) {
+        public ZoneCornersInDifferentWorldsException(Location location1, Location location2) {
             super("The corners of a new zone were given in two different worlds!", "zone in two worlds", "Location #1: " + location1.toString(), "Location #2: "
                     + location2.toString());
         }
 
+    }
+
+    public int getBlockCountInZone() {
+        int xLength = this.high.getBlockX() - this.low.getBlockX();
+        int yLength = this.high.getBlockY() - this.low.getBlockY();
+        int zLength = this.high.getBlockZ() - this.low.getBlockZ();
+
+        return xLength * yLength * zLength;
+    }
+
+    public float getVolume() {
+        float xLength = this.high.getX() - this.low.getX();
+        float yLength = this.high.getY() - this.low.getY();
+        float zLength = this.high.getZ() - this.low.getZ();
+
+        return xLength * yLength * zLength;
     }
 
     // TODO: utilities
