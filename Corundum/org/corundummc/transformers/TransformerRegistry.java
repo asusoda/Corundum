f8d07dcec1e11ed2c21b6f7f7175b1b0388cface
Niadel <niadel@virginmedia.com>
2015-04-28 08:01:05 +0100

    Some work on filling out TransformerRegistry and things in the transformers package. Transformer registration now works... Sort of.

--- Corundum/org/corundummc/transformers/TransformerRegistry.java
+++ Corundum/org/corundummc/transformers/TransformerRegistry.java
@@ -1,5 +1,8 @@
 package org.corundummc.transformers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Where we internally register transformers.
  * 
@@ -7,5 +10,10 @@ package org.corundummc.transformers;
  */
 public class TransformerRegistry
 {
+	public List<BaseTransformer> transformers = new ArrayList<>();
 	
+	void registerTransformer(BaseTransformer transformer)
+	{
+		transformers.add(transformer);
+	}
 }
